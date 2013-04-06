cbf70604be07b311163a26c25fdb81eedac84468
sagardave <davesagar2012@gmail.com>
2013-04-06 19:57:21 -0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,18 @@
+//getting elements
+var pad = document.getElementById("pad");
+var ball = document.getElementById("ball");
+var svg = document.getElementById("svgRoot");
+var message = document.getElementById("message");
+
+//ball
+var ballRadius = ball.baseVal.value;
+var ballX;
+var ballY;
+var previousBallPosition = { x: o, y: 0 };
+var ballDirectionX;
+var ballDirectionY;
+var ballSpeed = 10;
+
+//pad
+
+
